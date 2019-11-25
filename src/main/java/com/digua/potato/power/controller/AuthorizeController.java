d694ecca9a7d2339d16eb9cc716b3e3914e933aa
yjf_home <ooohahafox@163.com>
2019-11-25 19:57:10 +0800

    变量配置 提取配置文件

--- src/main/java/com/digua/potato/power/controller/AuthorizeController.java
+++ src/main/java/com/digua/potato/power/controller/AuthorizeController.java
@@ -7,6 +7,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +22,12 @@ public class AuthorizeController {
 
     @Autowired
     private GitHubProvider gitHubProvider;
-
+    @Value("${github.client.id}")
+    private String  clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
     /**
      * gitHub第三方登录回调方法
      * @param code   返回的code
@@ -32,11 +38,11 @@ public class AuthorizeController {
     public String callBack(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("b6f8bbd65a6f8c18cadc");
-        accessTokenDTO.setClient_secret("ec27efb9b37e794db29708b5a823ad2addf96ef9");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost/callBack");
+        accessTokenDTO.setRedirect_uri(redirectUrl);
         //使用code 访问git  换取 access_token
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         //用返回的access_token 请求git 获取用户信息
